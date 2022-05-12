1dad94a7f92aff63c821b4416fc527e7493b56fe
Markus Nordin <belfegorc4@gmail.com>
2022-05-12 02:35:45 +0200

    La till Code behind

---
+++ BugTrackerUI/Pages/ProjectBugs.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BugTrackerUI.Pages
+{
+    public partial class ProjectBugs
+    {
+        [Parameter]
+        public int Id { get; set; }
+
+        public List<Bug> Bugs { get; set; }
+        protected List<Bug> TestData()
+        {
+            Data db = new Data();
+            Bugs = db.GetBugs().Where(x => x.ProjectId == Id).OrderBy(x => x.Priority).ToList();
+            return Bugs;
+        }
+    }
+}
